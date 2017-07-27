972d994ae4791bbb7a54ddfdf45c61a27b595ea1
vinitraj10 <ent.vinit01@gmail.com>
2017-07-27 23:33:47 +0530

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+function getEntrySources(sources) {
+    if (process.env.NODE_ENV !== 'production') {
+        sources.push('webpack/hot/only-dev-server');
+    }
+}
+
+BUILD_DIR = path.resolve(__dirname,'dist/js');
+DEV_DIR = path.resolve(__dirname,'src');
+
+var config = {
+	entry:DEV_DIR + '/index.js',
+	output:{
+		publicPath:"/dist",
+		path:BUILD_DIR,
+		filename:'bundle.min.js',
+	},
+	module:{
+		loaders:[
+			{
+				test:/\.jsx?/,
+				include:DEV_DIR,
+				loader:'babel-loader',
+				exclude:/node_modules/,
+			}
+		]
+	}
+};
+module.exports = config;
