1748b87ca28f05ce92e71308de93067294d460e1
vinitraj10 <ent.vinit01@gmail.com>
2017-07-29 00:05:20 +0530

    content component updated

--- src/components/content.js
+++ src/components/content.js
@@ -1,17 +1,31 @@
 import React from "react";
 
 export default class Content extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state={
+			value:''
+		}
+		this.onChangeInput=this.onChangeInput.bind(this)
+	}
 	render(){
 		return(
 			<div className="container">
 				<div className="row row-content">
 					<div className="col-xs-12">
 						<div className="well">
-							<p>Hey World I am content</p>
+							<input onChange={this.onChangeInput}/><br/>
+							<cite>{this.state.value}</cite>
 						</div>
 					</div>
 				</div>
 			</div>
-		)
+		);
+	}
+	onChangeInput(event){
+		this.setState({
+			value:event.target.value
+		})
+
 	}
 }
