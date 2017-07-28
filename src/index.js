6a6af5e59cd3ce24fa3cced51328dca82f7e91d8
vinitraj10 <ent.vinit01@gmail.com>
2017-07-28 13:31:34 +0530

    Footer component added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import {render} from "react-dom";
 import Header from "./components/header";
 import Content from "./components/content";
+import Footer from "./components/footer";
 render(
 	<div>
 		<Header/>
 		<Content/>
+		<Footer/>
 	</div>,
 	document.getElementById('root')
 );
