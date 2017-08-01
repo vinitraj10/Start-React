079c55f1cb4ee5119fe756fa8558fee6046b3093
vinitraj10 <ent.vinit01@gmail.com>
2017-08-01 22:19:56 +0530

    states updated for time components

--- src/Components/Content/Timer/time.js
+++ src/Components/Content/Timer/time.js
@@ -7,6 +7,20 @@ export default class Time extends React.Component{
 			date: new Date(),
 		}
 	}
+	componentDidMount() {
+		this.timerId = setInterval(
+			() => this.clock(),
+			1000
+		);
+	}
+	componentWillUnmount() {
+		clearInterval(this.timerId);
+	}
+	clock(){
+		this.setState( (prevState) => {
+			return {date:new Date()};
+		});
+	}
 	render(){
 		return (
 			<h1>{this.state.date.toLocaleTimeString()}</h1>
